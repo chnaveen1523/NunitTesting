8ccd6b4db0cd8313399fd0e3a346488903db34d9
bannu-code-star <nitinchiliveri007@gmail.com>
2021-09-20 20:19:33 +0530

    [Naveen] Adding NunitTesting code

---
+++ ToBinary.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitTesting_CS
+{
+    class toBinary
+    {
+        public static void ConversiontoBinary()
+        {
+            Console.WriteLine("Enter a decimal number");
+            int dec = Convert.ToInt32(Console.ReadLine());
+            int i;
+            //declaring a array to store binany 
+            int[] Array = new int[15];
+            for (i = 0; dec > 0; i++)
+            {
+                Array[i] = dec % 2;
+                dec = dec / 2;
+            }
+            Console.WriteLine("Binary of the given decimal is:");
+            for (i = i - 1; i >= 0; i--)
+            {
+                Console.Write(Array[i]);
+            }
+        }
+    }
+}
