80667181f34cace1c180435c917e82e35b21a809
bannu-code-star <nitinchiliveri007@gmail.com>
2021-09-20 20:14:15 +0530

    [Naveen] Added a code

---
+++ MonthlyPayment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitTesting.cs
+
+    class MonthlyPayment
+{
+    //Taking user input as principal,rate of intrest , no of years
+    public static void UserInput()
+    {
+        Console.WriteLine("Enter a principal");
+        double principal = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter a interest amount ");
+        double rate = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter a year");
+        double year = Convert.ToDouble(Console.ReadLine());
+        calculateMonthlyPay(principal, rate, year);
+    }
+
+    public static void calculateMonthlyPay(double principal, double rate, double year)
+    {
+        double s = Math.Round(1 + (rate / 1200), 4);
+        double t = 12 * year;
+        double monthlyPayment = principal * (Math.Pow(s, t)) - principal;
+        Console.WriteLine("Monthly payment " + Math.Round(monthlyPayment));
+    }
+}
+}
---
+++ SquareRoot.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitTesting.cs
+{
+    class SquareRoot
+    {
+        public static void sqrtRoot()
+        {
+            //N is the nuumber for which you are calculating squareroot
+            Console.WriteLine("Enter a number");
+            double N = Convert.ToDouble(Console.ReadLine());
+            //set L is the tolerance level
+            double L = 0.00001;
+            double t = N;
+            double sqRoot;
+            int count = 0;
+            while (true)
+            {
+                count++;
+                sqRoot = 0.5 * (t + (N / t));
+                if (Math.Abs(sqRoot - t) < L)
+                {
+                    break;
+                }
+                t = sqRoot;
+            }
+            Console.WriteLine("Root Value is :" + Math.Round(sqRoot, 2));
+        }
+    }
+}
