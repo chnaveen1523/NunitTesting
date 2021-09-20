6c9724430bba966ae4615105ce98c3fa8c9bb1a9
bannu-code-star <nitinchiliveri007@gmail.com>
2021-09-20 20:17:33 +0530

    [Naveen] Added a NunitTesting code

---
+++ TemperatureConversion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitTesting.cs
+{
+    class TemperatureConversion
+    {
+        //creating a method to take input from user
+        public static void UserInput()
+        {
+            Console.WriteLine("Enter the Celsius:");
+            double celcius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the Fahrenheit:");
+            double fahrenheit = Convert.ToDouble(Console.ReadLine());
+            tempConversion(celcius, fahrenheit);
+        }
+
+        public static void tempConversion(double celcius, double fahrenheit)
+        {
+            double celToFarhen = (celcius * 9 / 5) + 32;
+            double FarhenToCel = (fahrenheit - 32) * 5 / 9;
+            Console.WriteLine("Celusis To Fahrenheit Temperature : " + Math.Round(celToFarhen));
+            Console.WriteLine("Fahrenheit To Celusis Temperature : " + Math.Round(FarhenToCel));
+        }
+    }
+}
